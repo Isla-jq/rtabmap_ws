18a441adab45a600c5010e285696307c813153d8
1294644079@qq.com <1294644079@qq.com>
2025-07-20 16:11:36 +0800

    add laser

--- src/slam_launch/slam_launch/slam_launch.py
+++ src/slam_launch/slam_launch/slam_launch.py
@@ -48,7 +48,7 @@ def generate_launch_description():
         parameters=[
             {"usb_bus_no": -1},
             {"usb_path": "null"},
-            {"confiPath": "/home/isla/code/rtabmap_ws/src/ascamera/configurationfiles"},
+            {"confiPath": "/home/isla/rtabmap_ws/src/ascamera/configurationfiles"},
             {"color_pcl": False},
             {"pub_tfTree": True},
             {"depth_width": 640},
